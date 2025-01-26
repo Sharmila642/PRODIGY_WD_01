08bf663d8cdeb329ff0730da778daf0168afe79d
Sharmila642 <sharmilapandi642@gmail.com>
2025-01-26 18:59:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section id="home" className="section home">
+        <h1>Welcome to Home</h1>
+      </section>
+      <section id="about" className="section about">
+        <h1>About Us</h1>
+      </section>
+      <section id="services" className="section services">
+        <h1>Our Services</h1>
+      </section>
+      <section id="contact" className="section contact">
+        <h1>Contact Us</h1>
+      </section>
+    </div>
+  );
+};
+
+export default App;
